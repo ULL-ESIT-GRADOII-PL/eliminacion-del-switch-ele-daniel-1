acb9d3c694aaa6907e30d30edc655e0c042bbeef
Eleazar Díaz Delgado <eleazardzdo@gmail.com>
2016-03-10 18:40:10 +0000

    First working idea of  and  methods

--- assets/js/Medida.js
+++ assets/js/Medida.js
@@ -1,4 +1,23 @@
+/*
+ Pinsamiento POO
 
+ class Medida:
+      static converters { UnitType : { Char : Unit -> Object:matcher } };
+
+      addToConverter :: UnitType -> Unit -> func Matcher ()
+
+ class Temperatura(Medida):
+      call father
+      Temperatura (Unit)
+      addToConverter :: Unit -> func Matcher ()
+      matcher() :: String -> Object<:Temperatura
+
+ class Celsius(Temperatura(Unit)):
+      matcher :: String -> Object<:Celsius
+      to :: Int -> Int
+      from :: Int -> Int
+
+*/
 /*
  Haskell pinsamiento
 
@@ -19,73 +38,62 @@
     //
     // Para declarar un nuevo tipo de dato se debe heredar de la clase Medida e implementar
     // los métodos para convertirlos a la medida estandar de cada caso, la cual esta comentada
-    // en cada clase de medida
-    function Medida(claseMedida, possibleConvertions) {
-        // Vector de tipos de medida y sus conversiones
-        // Se considera que no se puede pasar de m a temperatura
-        if (!this.converters) {
-            this.converters = { claseMedida : possibleConvertions };  // quiero guardar nombreMedida, function para comprobar tipo que debe ser una clausura
-        }
-        else {
-            this.converters[claseMedida] = possibleConvertions;
-        }
-
-        // Las funciones son un monoide con:
-        // identity como valor unitario
-        // y la composición de funciones como append
-
-        // if (!tipo) {
-        //     var param = XRegExp(""
-        //                         + "(?<value>       [-+]?\\d+ (?:[\\.,]\\d*)?\\s* ) # Get number \n"
-        //                         + "((e(?<exponent> [-+]?\\d+)\\s*)?)               # Get Exponent \n"
-        //                         + "(?<measure>     [a-zA-Z]+)                      # Get kind");
-        //     var m = XRegexExp.exec(valor, param);
-        //     this.valor = parseFloat(m.value) * Math.pow(10, parseInt(m.exponent));
-        //     this.tipo  = m.measure;
-        // }
-        // else {
-        //     this.valor = valor;
-        //     this.tipo  = tipo;
-        // }
+    // en cada clase hija de medida
+    var converters = {};
+    function Medida() {
     }
 
     Medida.prototype = {
-        convertir: function (input) {
-            var measures = '[a-zA-Z]+';
-
-            var inputRegex = XRegExp(
-                      '^(\\s*)                                            # whitespaces \n'
-                    + '(?<value>       [-+]?\\d+ (?:[\\.,]\\d*)?\\s*)     # captures the number   \n'
-                    + '((e(?<exponent> [-+]?\\d+)\\s*)?)                  # captures the exponent \n'
-                    + '(?<kind>       ' + measures + ')                   # Capture kind of value \n'
-                    + '((?:\\s+to)?\\s+ (?<toKind>' + measures + '))?     # Get "to" syntax \n'
-                    + '(\\s*)$                                            # whitespaces \n'
-                    , 'xi');
-
-            var m = XRegexExp.exec(valor, param);
-
-            var medidas = Object.keys(this.converters); // [ 'Temperatura', 'Distancia' ... ]
-            var i = 0;
-            var found = false;
-            var from; // no se puede hacer var from = x && var to = y && false :(
-            var to;
-            while (!found && medidas[i]) { // i < medidas.length
-                // So funny
-                // Miro por categorias, si en esa consigo el to y el from para convertir, salgo
-                (from = this.converters[medidas[i]](m.kind)) && (to = this.converters[medidas[i]](m.kind)) && (found = true);
-                i++;
-            }
-
-            if (found) {
-                var result = to(from(value)); // esta compuesto >>> queda mejor
-                return result;
-            }
-            else if (from !== null) { // esta mal pensado repensar o añadir propiedades al from TODO:
-                return "No se encontro un valor para convertir de " + "joder" + "se tienen las siguientes opciones" + "joder" + " el tipo de medida es " + "joder";
+        addToConverters: function (unit_type, unit, obj) {
+            if (!converters[unit_type]) {
+                converters[unit_type] = {};
+                converters[unit_type][unit] = obj;
             }
             else {
-                return "No se sabe en que medidas se trabaja";
+                converters[unit_type][unit] = obj;
             }
+        }
+    };
+
+    Medida.convertir = function (input) {
+        var measures = '[a-zA-Z]+';
+
+        var inputRegex = XRegExp(
+            '^(\\s*)                                            # whitespaces \n'
+                + '(?<value>       [-+]?\\d+ (?:[\\.,]\\d*)?\\s*)     # captures the number   \n'
+                + '((e(?<exponent> [-+]?\\d+)\\s*)?)                  # captures the exponent \n'
+                + '(?<kind>       ' + measures + ')                   # Capture kind of value \n'
+                + '((?:\\s+to)?\\s+ (?<toKind>' + measures + '))?     # Get "to" syntax \n'
+                + '(\\s*)$                                            # whitespaces \n'
+            , 'xi');
+
+        var m = XRegExp.exec(input, inputRegex);
+
+        var medidas = Object.keys(converters); // [ 'Temperatura', 'Distancia' ... ]
+        var i = 0;
+        var found = false;
+        var from; // no se puede hacer var from = x && var to = y && false :(
+        var to;
+        while (!found && medidas[i]) { // i < medidas.length
+            // So funny
+            // Miro por categorias, si en esa consigo el to y el from para convertir, salgo
+            // if ()
+            (from = converters[medidas[i]][m.kind[0]](m.kind).from)
+                && (to = converters[medidas[i]][m.toKind[0]](m.toKind).to)
+                && (found = true);
+            i++;
+        }
+
+        if (found) {
+            // measure.constructor.name
+            var result = to(from(parseInt(m.value))); // esta compuesto >>> queda mejor
+            return result;
+        }
+        else if (from !== null) { // esta mal pensado repensar o añadir propiedades al from TODO:
+            return "No se encontro un valor para convertir de " + "joder" + "se tienen las siguientes opciones" + "joder" + " el tipo de medida es " + "joder";
+        }
+        else {
+            return "No se sabe en que medidas se trabaja";
         }
     };
 
--- assets/js/Temperatura.js
+++ assets/js/Temperatura.js
@@ -9,32 +9,58 @@
     // y donde para el caso de las temperaturas
     // toCelsius = toStd
     // fromCelsius = fromStd
-
-    function Temperatura(identificador, obj) {
-        Medida.call(this, "Temperatura",
-                    function(value) { return this.convertions; });
-
-
-        console.log(Medida.prototype.converters);
-        if (this.convertions) {
-
-            this.convertions = {}
-        }
-        //this.posibleConvertions
+    function Temperatura(unit, matcher) {
+        Medida.call(this);
+        Medida.prototype.addToConverters.call(this, "Temperatura", unit, matcher);
     }
 
 
-
-    // Debido a esto las 
+    // Las clases hijas de los tipos medidas deben implementar, una funcion de
+    // matcheado `matcher`, y la transformación de la medida estandar a la
+    // a la implemetada y al revés `to` y `from`
+    // el nombre del constructor debe ser descriptivo
     function Celsius() {
+        Temperatura.call(this, 'c', function(value) {
+            if (value.match(/(c(?:e(?:l(?:s(?:i(?:u(?:s)?)?)?)?)?)?)/)) {
+                return new Celsius();
+            }
+            else {
+                throw "No se pudo convetir";
+            }
+        });
     }
 
     Celsius.prototype = {
-        toStd: function(value) {
+        to: function(value) {
             return value;
         },
-        fromStd: function(value) {
+
+        from: function(value) {
             return value;
         }
     };
+    new Celsius(); // creamos un singleton privado
+
+    function Fahrenheit() {
+        Temperatura.call(this, 'f', function(value) {
+            if (value.match(/^(f(?:a(?:h(?:r(?:e(?:n(?:h(?:e(?:i(?:t)?)?)?)?)?)?)?)?)?)$/)) {
+                return new Fahrenheit();
+            }
+            else {
+                throw "No se pudo convetir";
+            }
+        });
+    }
+
+    Fahrenheit.prototype = {
+        to: function(value) {
+            return value;
+        },
+
+        from: function(value) {
+            return value;
+        }
+    };
+    new Fahrenheit(); // Otro mas privado
+
 })(this);
